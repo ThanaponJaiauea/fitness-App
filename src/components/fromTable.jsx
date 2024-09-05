dbe86c8bf94f86858c2bbeaa277b5c13cf6b9bfb
ThanaponJaiauea <ham@yojoies.com>
2024-09-05 16:15:20 +0700

    #

--- src/components/fromTable.jsx
+++ src/components/fromTable.jsx
@@ -7,6 +7,7 @@ export default function FromTable({data, dayKey}) {
   const numberOfSets = 8
 
   const [openSeeMore, setOpenSeeMore] = useState(false)
+  const [selectedExercise, setSelectedExercise] = useState(null)
 
   // Load saved data from localStorage for Monday
   const initialInputs = () => {
@@ -78,7 +79,10 @@ export default function FromTable({data, dayKey}) {
                 {el.title}
 
                 <button
-                  onClick={() => setOpenSeeMore(!openSeeMore)}
+                  onClick={() => {
+                    setSelectedExercise(el)
+                    setOpenSeeMore(true)
+                  }}
                   className="hover:text-red-400">
                   ดูเพิ่มเติม
                 </button>
@@ -110,13 +114,13 @@ export default function FromTable({data, dayKey}) {
         </tbody>
       </table>
 
-      {openSeeMore && (
+      {openSeeMore && selectedExercise && (
         <div className=" overflow-y-auto fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
           <div className="relative p-4 w-full max-w-2xl max-h-full">
             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
               <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                 <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                  name id
+                  {selectedExercise.title}
                 </h3>
 
                 <button
@@ -132,9 +136,9 @@ export default function FromTable({data, dayKey}) {
                     viewBox="0 0 14 14">
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
                     />
                   </svg>
